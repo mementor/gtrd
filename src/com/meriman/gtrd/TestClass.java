17394ba314254dcc6b0f6eca914d02d105ef8403
meritman <debiboon@gmail.com>
2012-10-05 13:57:30 +0400

    Testing commit

---
+++ src/com/meriman/gtrd/TestClass.java
@@ -0,0 +1,12 @@
+package com.meriman.gtrd;
+
+/**
+ * User: Mentor <debiboon@gmail.com>
+ * Date: 05.10.12
+ * Time: 13:53
+ */
+public class TestClass {
+    public static void main (String[] args) {
+        System.out.println("Yyyyaaaaayyy!");
+    }
+}
