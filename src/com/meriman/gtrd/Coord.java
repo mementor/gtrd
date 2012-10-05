0d1a23512b44e06c408529b0e9d2f6a5c3974c39
meritman <debiboon@gmail.com>
2012-10-05 14:16:49 +0400

    Added .gitignore and couple of classes

---
+++ src/com/meriman/gtrd/Coord.java
@@ -0,0 +1,11 @@
+package com.meriman.gtrd;
+
+/**
+ * User: Mentor <debiboon@gmail.com>
+ * Date: 05.10.12
+ * Time: 14:02
+ */
+public class Coord {
+    public int x;
+    public int y;
+}
---
+++ src/com/meriman/gtrd/Entity.java
@@ -0,0 +1,14 @@
+package com.meriman.gtrd;
+
+/**
+ * User: Mentor <debiboon@gmail.com>
+ * Date: 05.10.12
+ * Time: 14:02
+ */
+public class Entity {
+    private Coord coords;
+
+    public Entity(Coord c) {
+        coords = c;
+    }
+}
